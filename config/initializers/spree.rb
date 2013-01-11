028ffe8276e1cb4ebc1149c0df261d73ffacfc5e
Christoph Bünte <info@christophbuente.de>
2013-01-11 13:54:55 +0100

    Add more default settings.

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -8,8 +8,8 @@
 Spree.config do |config|
   # Example:
   # Uncomment to override the default site name.
-  # config.site_name = "Spree Demo Site"
-  config.default_locale = 'de'
+  config.site_name = "MeinKleineFarm.org"
+#  config.default_locale = 'de'
   config.allow_ssl_in_production = false
   config.currency = 'EUR'
   config.display_currency = true
