f6b68c763ac0d8184a1f3cb23485a164a8f8f3b2
Christoph Bünte <info@christophbuente.de>
2013-03-18 11:21:00 +0100

    latest theme version.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130315115832) do
+ActiveRecord::Schema.define(:version => 20130315141034) do
 
   create_table "spree_activators", :force => true do |t|
     t.string   "description"
@@ -249,6 +249,27 @@ ActiveRecord::Schema.define(:version => 20130315115832) do
 
   add_index "spree_orders", ["number"], :name => "index_spree_orders_on_number"
 
+  create_table "spree_pages", :force => true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.string   "slug"
+    t.datetime "created_at",                                  :null => false
+    t.datetime "updated_at",                                  :null => false
+    t.boolean  "show_in_header",           :default => false, :null => false
+    t.boolean  "show_in_footer",           :default => false, :null => false
+    t.string   "foreign_link"
+    t.integer  "position",                 :default => 1,     :null => false
+    t.boolean  "visible",                  :default => true
+    t.string   "meta_keywords"
+    t.string   "meta_description"
+    t.string   "layout"
+    t.boolean  "show_in_sidebar",          :default => false, :null => false
+    t.string   "meta_title"
+    t.boolean  "render_layout_as_partial", :default => false
+  end
+
+  add_index "spree_pages", ["slug"], :name => "index_pages_on_slug"
+
   create_table "spree_payment_methods", :force => true do |t|
     t.string   "type"
     t.string   "name"
@@ -446,6 +467,7 @@ ActiveRecord::Schema.define(:version => 20130315115832) do
   create_table "spree_schweine", :force => true do |t|
     t.string   "name"
     t.text     "description"
+    t.date     "date_of_birth"
     t.date     "date_of_death"
     t.integer  "weight"
     t.string   "meta_keywords"
