8073b95a100b9f63d66ccf2928c78863d11affce
Christoph Bünte <info@christophbuente.de>
2013-04-12 18:46:18 +0200

    Precompile tablesorter js manually.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Shop::Application.configure do
   config.action_controller.asset_host = "http://railsausbildung.de"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( admin/jquery.tablesorter.min.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
