1e2284c3d545dd04b075f67e6030c3ad14e8a830
Christoph Bünte <info@christophbuente.de>
2013-07-08 14:37:12 +0200

    Add guard and fix factories.

--- spec/factories/spree_shipping_categories.rb
+++ spec/factories/spree_shipping_categories.rb
@@ -1,6 +1,7 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :order, :class => 'Spree::Order' do
+  factory :shipping_category, :class => 'Spree::ShippingCategory' do
+    name 'Standardversand'
   end
 end
