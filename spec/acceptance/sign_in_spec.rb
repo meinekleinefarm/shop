25d2ebef7b5c0ec994053f296dd25123af63d636
Christoph Bünte <info@christophbuente.de>
2013-07-15 16:48:16 +0200

    Bundle again wit non-local paths

--- spec/acceptance/sign_in_spec.rb
+++ spec/acceptance/sign_in_spec.rb
@@ -5,9 +5,9 @@ feature "Sign in", %q{
   As a customer
   I want to sign in
 } do
- 
+
   background do
-    FactoryGirl.create(:admin, email: "admin@example.com", password: "secret_password", password_confirmation: "secret_password")
+    FactoryGirl.create(:admin_user, email: "admin@example.com", password: "secret_password", password_confirmation: "secret_password")
   end
 
   scenario "Sign in" do
