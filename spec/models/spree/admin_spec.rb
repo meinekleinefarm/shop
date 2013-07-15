a4e25a7065b03ee8250dd86c7bd1c4dafa7bc40d
Christoph Bünte <info@christophbuente.de>
2013-07-15 16:48:16 +0200

    Bundle again wit non-local paths

--- spec/models/spree/admin_spec.rb
+++ spec/models/spree/admin_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 
 describe Spree::User do
 
-  subject { FactoryGirl.build(:admin) }
-  
+  subject { FactoryGirl.build(:admin_user) }
+
   context :validations do
-    
+
     it { should be_valid }
-    
+
   end
 end
--- spec/models/spree/order_spec.rb
+++ spec/models/spree/order_spec.rb
@@ -2,8 +2,16 @@
 require 'spec_helper'
 describe Spree::Order do
 
+  let :bill_address do
+    FactoryGirl.create(:address)
+  end
+
+  let :ship_address do
+    FactoryGirl.create(:address)
+  end
+
   subject do
-    FactoryGirl.build(:order)
+    FactoryGirl.build(:order, :total => 120.0, bill_address: bill_address, ship_address: ship_address)
   end
 
   context :validations do
@@ -12,24 +20,78 @@ describe Spree::Order do
 
   end
 
-  context :new_order do
-
-    subject do
-      FactoryGirl.build(:new_order)
-    end
-
-    it "should render csv" do
-      subject.to_csv.should eql ""
-    end
-  end
-
   context :completed_order do
-    subject do
-      FactoryGirl.build(:completed_order)
+
+    before :each do
+      subject.save!
+      FactoryGirl.create(:line_item, :order => subject)
     end
 
+    #{
+    # id
+    # status
+    # billing_salutation
+    # billing_first_name
+    # billing_last_name
+    # billing_zip
+    # billing_city
+    # billing_street
+    # billing_street_nr
+    # billing_email
+    # billing_phone
+    # shipping_salutation
+    # shipping_first_name
+    # shipping_last_name
+    # shipping_zip
+    # shipping_city
+    # shipping_street
+    # shipping_street_nr
+    # shipping_email
+    # shipping_phone
+    # shipping_address_is_billing_address
+    # is_gift
+    # newsletter_subscription
+    # price_products_total_with_tax
+    # price_products_total_without_tax
+    # price_products_total_tax
+    # price_shipping_total
+    # price_total
+    # weight_total_in_gram
+    # weight_total_in_gram_brutto
+    # created_at_date
+    # created_at_time
+    # updated_at_date
+    # updated_at_time
+    # cart_text
+    # comment
+    #}
+
     it "should render csv" do
-      subject.to_csv.should eql ""
+      subject.to_csv.should eql [
+        "1",
+        nil,
+        "Herr/Frau",
+        "John",
+        "Doe",
+        "20170",
+        "Herndon",
+        "10 Lovely Street",
+        nil,
+        "#{subject.email}",
+        "123-456-7890",
+        "Herr/Frau",
+        "John",
+        "Doe",
+        "Herndon",
+        "10 Lovely Street",
+        nil,
+        "#{subject.email}",
+        "123-456-7890",
+        "Nein",
+        "Nein",
+        "Nein",
+        "0.0"
+      ]
     end
   end
 end
