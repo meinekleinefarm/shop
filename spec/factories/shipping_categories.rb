cf6d45fa896f0b555945947743540f92d8c81de2
Christoph Bünte <info@christophbuente.de>
2013-07-26 15:38:46 +0200

    Remove factories.

--- spec/factories/shipping_categories.rb
+++
@@ -1,7 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :shipping_category, :class => 'Spree::ShippingCategory' do
-    name 'Standardversand'
-  end
-end
