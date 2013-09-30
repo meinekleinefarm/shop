d3578cd25a0c2c02f818a9112830ea7aa5283a56
Christoph Bünte <info@christophbuente.de>
2013-09-30 14:08:05 +0200

    Move stuff to theme extension.

--- app/overrides/add_teaser_to_start_page.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path => 'spree/layouts/spree_application',
-                     :name => 'add_main_nav_bar_as_first_child_of_wrapper',
-                     :insert_before => '#content',
-                     :erb => '<%= yield :teaser %>')
