e706571e362cfe15c13d72050c41fbf5a5f8bb11
Christoph Bünte <info@christophbuente.de>
2013-10-25 12:53:04 +0200

    Add rules for german inflection.

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -6,6 +6,7 @@ ActiveSupport::Inflector.inflections do |inflect|
 #   inflect.plural /^(ox)$/i, '\1en'
 #   inflect.singular /^(ox)en/i, '\1'
   inflect.irregular 'schwein', 'schweine'
+  inflect.irregular 'rind', 'rinder'
 #   inflect.uncountable %w( fish sheep )
 end
 #
