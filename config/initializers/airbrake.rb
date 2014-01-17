750a4b0445c4706488453394a1e79f31a42118a0
Christoph Bünte <info@christophbuente.de>
2014-01-17 12:10:07 +0100

    Use separate logger for airbrake.

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,4 +1,5 @@
 Airbrake.configure do |config|
   config.api_key = '62c1649d6fe95c19e72043eb2bd90ad9'
   config.secure = true
+  config.logger = Logger.new(Rails.root.join('log', 'airbrake.log'))
 end
