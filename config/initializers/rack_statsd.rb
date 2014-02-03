218188a02d3fc819ca950e79aaf0847ff9e47d9f
Christoph Bünte <info@christophbuente.de>
2014-02-03 15:00:01 +0100

    Do not use statsd in test or development.

--- config/initializers/rack_statsd.rb
+++ config/initializers/rack_statsd.rb
@@ -1,2 +1,4 @@
-current_sha = `git rev-parse HEAD`[0..8]
-Rails.application.middleware.insert_before Rack::Lock, RackStatsD::ProcessUtilization, 'mkf-shop', current_sha
+if Rails.env.staging? or Rails.env.production?
+  current_sha = `git rev-parse HEAD`[0..8]
+  Rails.application.middleware.insert_before Rack::Lock, RackStatsD::ProcessUtilization, 'mkf-shop', current_sha
+end
