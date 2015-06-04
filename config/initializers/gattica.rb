96e7fb929b183df91bb8ed8aca1c5c920707ae5c
Christoph Bünte <info@christophbuente.de>
2015-06-04 23:21:47 +0200

    Remove gattica.

--- config/initializers/gattica.rb
+++ config/initializers/gattica.rb
@@ -2,9 +2,9 @@ require 'ostruct'
 require 'yaml'
 gattica_config = YAML.load_file("#{Rails.root}/config/gattica.yml")
 GatticaConfig = OpenStruct.new gattica_config[Rails.env]
-GATTICA_INSTANCE = Gattica.new({
-                                email:      GatticaConfig.email,
-                                password:   GatticaConfig.password,
-                                profile_id: GatticaConfig.profile_id
-                              })
+#GATTICA_INSTANCE = Gattica.new({
+#                                email:      GatticaConfig.email,
+#                                password:   GatticaConfig.password,
+#                                profile_id: GatticaConfig.profile_id
+#                              })
 
